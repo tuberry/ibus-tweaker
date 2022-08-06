b4cf4e993edb12dd01a5b0115731a6b4e994d64d
tuberry <17917040+tuberry@users.noreply.github.com>
2022-08-06 19:21:10 +0800

    cleanup
    cleanup: fields in prefs.js
    typo: fix typo in ui.js

--- ibus-tweaker@tuberry.github.com/prefs.js
+++ ibus-tweaker@tuberry.github.com/prefs.js
@@ -28,62 +28,45 @@ class IBusTweakerPrefs extends Adw.PreferencesGroup {
     constructor() {
         super();
         this._buildWidgets();
-        this._bindValues();
         this._buildUI();
     }
 
     _buildWidgets() {
-        this._field_enable_clip   = new Gtk.CheckButton();
-        this._field_enable_dialog = new Gtk.CheckButton();
-        this._field_enable_font   = new Gtk.CheckButton();
-        this._field_enable_input  = new Gtk.CheckButton();
-        this._field_enable_orien  = new Gtk.CheckButton();
-        this._field_enable_theme  = new Gtk.CheckButton();
-        this._field_page_button   = new Gtk.CheckButton();
-        this._field_clip_page     = new UI.Spin(4, 10, 1, _('Page size'));
-        this._field_clip_history  = new UI.Short(gsettings, Fields.CLIPHISTCUT);
-        this._field_orientation   = new UI.Drop(_('Vertical'), _('Horizontal'));
-        this._field_run_dialog    = new UI.Short(gsettings, Fields.RUNSHORTCUT);
-        this._field_unknown_mode  = new UI.Drop(_('On'), _('Off'), _('Default'));
-        this._field_custom_font   = new Gtk.FontButton({ valign: Gtk.Align.CENTER });
-        this._field_theme_style   = new UI.Drop(_('Auto'), _('Light'), _('Dark'), _('System'));
-        this._field_theme_color   = new UI.Drop(_('Red'), _('Green'), _('Orange'), _('Blue'), _('Purple'), _('Turquoise'), _('Grey'));
+        this._field_clip_history = new UI.Short(gsettings, Fields.CLIPHISTCUT);
+        this._field_run_dialog   = new UI.Short(gsettings, Fields.RUNSHORTCUT);
+        this._field = {
+            ENABLECLIP:    ['active',   new Gtk.CheckButton()],
+            USECUSTOMFONT: ['active',   new Gtk.CheckButton()],
+            AUTOSWITCH:    ['active',   new Gtk.CheckButton()],
+            ENABLEDIALOG:  ['active',   new Gtk.CheckButton()],
+            ENABLEMSTHEME: ['active',   new Gtk.CheckButton()],
+            ENABLEORIEN:   ['active',   new Gtk.CheckButton()],
+            PAGEBUTTON:    ['active',   new Gtk.CheckButton()],
+            CLIPPAGESIZE:  ['value',    new UI.Spin(4, 10, 1, _('Page size'))],
+            ORIENTATION:   ['selected', new UI.Drop(_('Vertical'), _('Horizontal'))],
+            UNKNOWNMODE:   ['selected', new UI.Drop(_('On'), _('Off'), _('Default'))],
+            CUSTOMFONT:    ['font',     new Gtk.FontButton({ valign: Gtk.Align.CENTER })],
+            MSTHEMESTYLE:  ['selected', new UI.Drop(_('Auto'), _('Light'), _('Dark'), _('System'))],
+            MSTHEMECOLOR:  ['selected', new UI.Drop(_('Red'), _('Green'), _('Orange'), _('Blue'), _('Purple'), _('Turquoise'), _('Grey'))],
+        };
+        Object.entries(this._field).forEach(([x, [y, z]]) => gsettings.bind(Fields[x], z, y, Gio.SettingsBindFlags.DEFAULT));
+        this._field.AUTOSWITCH[1].bind_property('active', this._field.ENABLEDIALOG[1], 'sensitive', GObject.BindingFlags.DEFAULT);
+        this._field.AUTOSWITCH[1].connect('notify::active', widget => {
+            this._field_run_dialog.set_sensitive(widget.active && this._field.ENABLEDIALOG[1].active);
+        });
+        this._field.ENABLEDIALOG[1].set_sensitive(this._field.AUTOSWITCH[1].active);
+        this._field_run_dialog.set_sensitive(this._field.AUTOSWITCH[1].active && this._field.ENABLEDIALOG[1].active);
     }
 
     _buildUI() {
         [
-            [this._field_page_button, [_('Hide page buttons')]],
-            [this._field_enable_orien, [_('Candidates orientation')], this._field_orientation],
-            [this._field_enable_input, [_('Autoswitch input mode')], this._field_unknown_mode],
-            [this._field_enable_dialog, [_('Run dialog')], this._field_run_dialog],
-            [this._field_enable_theme, [_('MS IME theme')], this._field_theme_style, this._field_theme_color],
-            [this._field_enable_clip, [_('Clipboard history'), _('Depends on python-pinyin for Chinese search')], this._field_clip_page, this._field_clip_history],
-            [this._field_enable_font, [_('Use custom font')], this._field_custom_font],
+            [this._field.PAGEBUTTON[1],    [_('Hide page buttons')]],
+            [this._field.ENABLEORIEN[1],   [_('Candidates orientation')], this._field.ORIENTATION[1]],
+            [this._field.AUTOSWITCH[1],    [_('Autoswitch input mode')], this._field.UNKNOWNMODE[1]],
+            [this._field.ENABLEDIALOG[1],  [_('Run dialog')], this._field_run_dialog],
+            [this._field.ENABLEMSTHEME[1], [_('MS IME theme')], this._field.MSTHEMESTYLE[1], this._field.MSTHEMECOLOR[1]],
+            [this._field.ENABLECLIP[1],    [_('Clipboard history'), _('Depends on python-pinyin for Chinese search')], this._field.CLIPPAGESIZE[1], this._field_clip_history],
+            [this._field.USECUSTOMFONT[1], [_('Use custom font')], this._field.CUSTOMFONT[1]],
         ].forEach(xs => this.add(new UI.PrefRow(...xs)));
     }
-
-    _bindValues() {
-        [
-            [Fields.AUTOSWITCH,    this._field_enable_input,  'active'],
-            [Fields.ENABLEDIALOG,  this._field_enable_dialog, 'active'],
-            [Fields.ENABLEMSTHEME, this._field_enable_theme,  'active'],
-            [Fields.ENABLEORIEN,   this._field_enable_orien,  'active'],
-            [Fields.MSTHEMECOLOR,  this._field_theme_color,   'selected'],
-            [Fields.MSTHEMESTYLE,  this._field_theme_style,   'selected'],
-            [Fields.ORIENTATION,   this._field_orientation,   'selected'],
-            [Fields.UNKNOWNMODE,   this._field_unknown_mode,  'selected'],
-            [Fields.PAGEBUTTON,    this._field_page_button,   'active'],
-            [Fields.ENABLECLIP,    this._field_enable_clip,   'active'],
-            [Fields.USECUSTOMFONT, this._field_enable_font,   'active'],
-            [Fields.CLIPPAGESIZE,  this._field_clip_page,     'value'],
-            [Fields.CUSTOMFONT,    this._field_custom_font,   'font'],
-        ].forEach(xs => gsettings.bind(...xs, Gio.SettingsBindFlags.DEFAULT));
-        this._field_enable_input.bind_property('active', this._field_enable_dialog, 'sensitive', GObject.BindingFlags.DEFAULT);
-        this._field_enable_input.connect('notify::active', widget => {
-            this._field_run_dialog.set_sensitive(widget.active && this._field_enable_dialog.active);
-        });
-        this._field_enable_dialog.set_sensitive(this._field_enable_input.active);
-        this._field_run_dialog.set_sensitive(this._field_enable_input.active && this._field_enable_dialog.active);
-    }
 }
-
